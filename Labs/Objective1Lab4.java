462c56d0455000e47468664346691dc970e03151
Brianna Hickerson <brehickerson@yahoo.com>
2022-02-08 01:40:47 -0800

    complete objective1lab4

---
+++ Labs/Objective1Lab4.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Objective1Lab4 {
+  public static void main(String [] args) {
+
+    Scanner Keyboard = new Scanner(System.in);
+
+    System.out.println("Brianna");
+
+    System.out.println("Hickerson");
+
+    System.out.println("Octopus");
+
+    System.out.println("Bbq");
+
+    System.out.println("No Scrubs");
+
+    System.out.println("Brianna" + "Hickerson" + "Octopus" + "Bbq" + "No Scrubs");
+
+  }
+}
