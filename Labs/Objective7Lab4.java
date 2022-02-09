4604f238b37024dffcbb45eb94aa3185625878aa
Brianna Hickerson <brehickerson@yahoo.com>
2022-02-09 05:30:21 -0800

    complete objective7lab1

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,12 @@
+public class Objective7lab1 {
+  public static void main(String[] args) {
+    int counter = 20;
+
+    while(counter > 0) {
+      System.out.println("1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10" + num);
+      num = num + 1;
+
+      counter = counter - 1;
+    }
+  }
+}
