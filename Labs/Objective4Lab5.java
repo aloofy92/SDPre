7bb3da5c998a776c7b94a99a3bd473f81dc7102c
Brianna Hickerson <brehickerson@yahoo.com>
2022-02-09 03:01:25 -0800

    complete Objective4Lab5

---
+++ Labs/Objective4Lab5.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Objective4Lab4 {
+  public static void main(String [] args) {
+    Scanner scanner = new Scanner(System.in);
+    int num = 6;
+
+    if(6 % 2 == 0){
+      System.out.println(" 6 is even ");
+    }
+
+    }
+  }
