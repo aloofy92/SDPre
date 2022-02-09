51065ff75e85e24c4641d6242095abd6f1e9675d
Brianna Hickerson <brehickerson@yahoo.com>
2022-02-09 02:38:34 -0800

    complete objective3lab5

---
+++ Labs/Objective3Lab5.java
@@ -0,0 +1,15 @@
+public class Objective3Lab5 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int num = 3;
+
+    if(3 > 0) {
+     System.out.println(" 3 is greater than 0 ");
+
+
+
+    scanner.close();
+  }
+
+ }
+}
