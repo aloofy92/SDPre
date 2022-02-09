0d03799afbd16a16c4a5cd48ab1b78ec699f63cb
Brianna Hickerson <brehickerson@yahoo.com>
2022-02-09 05:45:54 -0800

    complete objective8lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,21 @@
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+
+    int counter = 10;
+
+    while(counter > 0) {
+      System.out.println("10" + num);
+      num = num + 1;
+
+      counter = counter - 1;
+
+      int counter = 10;
+
+      while(counter > 0) {
+        System.out.println("10" - num);
+        num = num - 1;
+
+        counter = counter - 1;
+
+  }
+}
