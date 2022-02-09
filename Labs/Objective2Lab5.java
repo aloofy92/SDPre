8a97e2216ccd0c429fc8ac1d94315faa14674255
Brianna Hickerson <brehickerson@yahoo.com>
2022-02-09 02:10:52 -0800

    complete objective2lab5

---
+++ Labs/Objective2Lab5.java
@@ -0,0 +1,18 @@
+public class Objective2Lab5 {
+  public static void main(String[] args) {
+    String string1 = "Banana";
+    String string2 = "Bacon";
+    String string3 = "Banana";
+
+    if(Banana.equals(Bacon)) {
+      System.out.println( Banana + "is equivalent to" + Bacon);
+    }
+    else {
+      System.out.println(Banana + "is not equivalent to" + Bacon);
+    }
+    if (Banana.equals(Banana)) {
+      System.out.println(Banana + "is equivalent to" + Banana);
+    }
+
+  }
+}
