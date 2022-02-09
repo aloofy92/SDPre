094c5c9bf738e36d1a11d36e44c7ba48b14eeaea
Brianna Hickerson <brehickerson@yahoo.com>
2022-02-09 01:01:45 -0800

    complete objectivelab5

---
+++ Labs/Objective1Lab5.java
@@ -0,0 +1,16 @@
+public class Objective1Lab5 {
+  public static void main(String[] args) {
+    int num1 = 5;
+    int num2 = 15;
+
+
+    if(5 > 15) {
+      System.out.println(" 5 is greater than 15");
+    }
+    else if(15 > 5) {
+      System.out.println(" 15 is greater than 5");
+
+    }
+
+  }
+}
