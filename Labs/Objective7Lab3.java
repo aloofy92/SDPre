f448d66e8ae3fcba3963c3a8189c7a932ee034ff
Brianna Hickerson <brehickerson@yahoo.com>
2022-02-09 05:23:23 -0800

    complete Objective7Lab3

---
+++ Labs/Objective7Lab3.java
@@ -0,0 +1,12 @@
+public class Objective7Lab3 {
+  public static void main(String[] args) {
+    int counter =1;
+
+    while(counter 1 -> 20)  {
+      if(1->20){
+        System.out.println(1 +"20" + counter);
+      }
+      else {
+
+  }
+}
